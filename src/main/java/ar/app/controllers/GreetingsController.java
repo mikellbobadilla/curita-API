5b717b2eeeb6668cd8dbe4d4f0c62e7fb2158ec2
Sting <bobadillasting@gmail.com>
2024-01-31 16:44:12 -0300

    add more methods at GreetingsController

--- src/main/java/ar/app/controllers/GreetingsController.java
+++ src/main/java/ar/app/controllers/GreetingsController.java
@@ -3,6 +3,7 @@ package ar.app.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,34 @@ public class GreetingsController {
 
     @GetMapping
     public ResponseEntity<Map<String, String>> greetings() {
-        return new ResponseEntity<>(Map.of("message", "Hello, world!"), HttpStatus.OK);
+
+        try {
+            // Aqui va la logica de negocio
+            throw new Exception("Error de negocio");
+        } catch(Exception exc) {
+            // Aqui va la logica de manejo de errores
+            return new ResponseEntity<>(Map.of("error", "Hay un problema!" + exc.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Dentro de la salida de datos de Java. Tienes lo que es la salida estandar y lo que es la salid por flujo
+     * @return
+     */
+    @GetMapping("/despacito")
+    public ResponseEntity<Map<String, String>> despacito() {
+        System.out.println("Despacito!");
+        return new ResponseEntity<>(Map.of("message", "Despacito!"), HttpStatus.OK);
+    }
+
+    /**
+     * Crear un recurso en el servidor
+     * @return
+     */
+    @PostMapping("/create")
+    public ResponseEntity<Map<String, String>> create() {
+        // Aqui se crea el recurso
+        // La logica de negocio
+        return new ResponseEntity<>(Map.of("message", "Create!"), HttpStatus.CREATED);
     }
 }

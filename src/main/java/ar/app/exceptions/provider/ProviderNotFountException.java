c465b8e4c7718ccf73b1e4264b2c265a42071aa1
Sting <bobadillasting@gmail.com>
2024-02-18 21:08:15 -0300

    feat: Add provider not found exception

---
+++ src/main/java/ar/app/exceptions/provider/ProviderNotFountException.java
@@ -0,0 +1,8 @@
+package ar.app.exceptions.provider;
+
+public class ProviderNotFountException extends ProviderException {
+
+    public ProviderNotFountException(String message) {
+        super(message);
+    }
+}

792144a129b09f16a4b1baf2001a333a461c5403
Sting <bobadillasting@gmail.com>
2024-02-18 21:04:39 -0300

    fix: Fix provider response class to record

--- src/main/java/ar/app/dto/provider/ProviderResponse.java
+++ src/main/java/ar/app/dto/provider/ProviderResponse.java
@@ -1,24 +1,17 @@
 package ar.app.dto.provider;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ProviderResponse {
-        Long id;
-        String name;
-        String address;
-        String email;
-        String contactProvider;
-        String phone;
-        String cuil;
-        String startOperations;
-        String observation;
+public record ProviderResponse(
+        Long id,
+        String name,
+        String address,
+        String email,
+        String contactProvider,
+        String phone,
+        String cuil,
+        String startOperations,
+        String observation
+) {
 }

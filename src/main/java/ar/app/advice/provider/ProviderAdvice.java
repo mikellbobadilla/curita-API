1e50e583663d9341ba29b94b7e88a149260cba3e
Sting <bobadillasting@gmail.com>
2024-02-18 21:05:55 -0300

    feat: Add exception handing Provider

--- src/main/java/ar/app/advice/provider/ProviderAdvice.java
+++ src/main/java/ar/app/advice/provider/ProviderAdvice.java
@@ -1,5 +1,9 @@
 package ar.app.advice.provider;
 
+import ar.app.advice.ErrorResponse;
+import ar.app.exceptions.provider.ProviderException;
+import ar.app.exceptions.provider.ProviderNotFountException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,5 +22,23 @@ public class ProviderAdvice {
             errors.put(error.getField(), error.getDefaultMessage());
         });
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ProviderException.class)
+    ResponseEntity<ErrorResponse> providerException(ProviderException exc) {
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(exc.getMessage())
+                .build();
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(ProviderNotFountException.class)
+    ResponseEntity<ErrorResponse> providerNotFoundException(ProviderNotFountException exc) {
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .error(exc.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
